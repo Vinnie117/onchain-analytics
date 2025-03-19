92bc9259a5daf1fb568c2348290465f54877c16d
Vinnie117 <hhang@hotmail.de>
2025-03-19 22:07:03 +0100

    add scatter matrix data

--- backend/scrape.py
+++ backend/scrape.py
@@ -30,6 +30,8 @@ def scrape_data(user_input):
 
     # Output results
     df.to_json('static/data/rich_list.json', index=False, orient='records')
+    df_matrix = df[['Ins', 'HODL_Days', 'Outs']]
+    df_matrix.to_csv('static/data/scatter_matrix.csv', index=False)
     yield "data: Data scraping completed successfully.\n\n"
 
 
@@ -64,51 +66,3 @@ async def server_scrape_data(user_input):
 
     yield "data: Data scraping completed successfully.\n\n"
 
-
-# async def playwright_scrape_data(user_input):
-#     base_url = "https://bitinfocharts.com/top-100-richest-bitcoin-addresses{}.html"
-#     headers = ['', 'Address', 'Balance', '% of coins', 'First In', 'Last In', 'Ins', 'First Out', 'Last Out', 'Outs']
-#     list_table_id = ['tblOne', 'tblOne2']
-#     num_pages = math.ceil(int(user_input) / 100)
-
-#     yield "data: Starting the data scraping process...\n\n"
-
-#     list_df = []
-
-#     async with async_playwright() as p: 
-#         browser = p.chromium.launch(headless=True)
-
-#         for i in range(1, num_pages + 1):
-#             page = browser.new_page()
-
-#             url = base_url.format("" if i == 1 else f"-{i}")
-#             page.goto(url)
-
-#             title = page.title()
-#             yield f"data: Scraping page {i} - {title}...\n\n"
-
-#             html = page.content()
-#             soup = BeautifulSoup(html, "html.parser")
-
-#             df_tblOne = _process_table(list_table_id[0], soup)
-#             df_tblOne2 = _process_table(list_table_id[1], soup, fallback_headers=headers)
-#             df_page = pd.concat([df_tblOne, df_tblOne2], ignore_index=True)
-
-#             list_df.append(df_page)
-
-#         browser.close()
-
-#     df = pd.concat(list_df, ignore_index=True)
-#     df = clean_data(df)
-#     df = make_features(df)
-    
-#     df = df[['Address', 'Short Address', 'BTC', 'USD Value', '% of coins',
-#              'First In', 'Last In', 'Ins', 'First Out', 'Last Out', 'Outs',
-#              'Days Since First In', 'Days Since Last In', 'Days Since Last Out',
-#              'Address Type', 'Txs Difference', 'Days Out Minus In',
-#              'HODL_Days', 'Age Band']]
-#     df = df.head(int(user_input))
-
-#     print(df)
-
-#     yield "data: Data scraping completed successfully.\n\n"
